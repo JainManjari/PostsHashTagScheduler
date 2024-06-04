257a1f65673c607beb427d49b4154123d5adbf1b
Manjari Jain <manjjain@paypal.com>
2024-06-04 16:16:29 +0530

    added views, assets, controllers and routes

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const path = require('path');
 const port = 8000;
 
 
 const app = express();
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.static("assets"));
+
+app.use("/", require("./routes"));
 
 
 app.listen(port, function(err){
