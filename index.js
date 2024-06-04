9ede26c816b9c73d33baa0cf114f8e5d9a41d94b
Manjari Jain <manjjain@paypal.com>
2024-06-04 16:07:45 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = 8000;
+
+
+const app = express();
+
+
+app.listen(port, function(err){
+    if(err) {
+        console.log("error in connecting with server ", err);
+        return;
+    }
+    console.log(`Server is working fine on port ${port}`);
+})
