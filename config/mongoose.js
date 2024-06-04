5b716e845ca7099016131099dc197c5241afbf77
Manjari Jain <manjjain@paypal.com>
2024-06-04 16:18:55 +0530

    added mongoose config

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const URL = `mongodb+srv://Cluster95362:WFNST3JmaVFd@cluster95362.ypsnut5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster95362`;
+
+mongoose.connect(URL);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "error connecting to db"));
+db.once("open", function () {
+  console.log("Successfully connected to mongodb");
+});
+module.exports = db;
